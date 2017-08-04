00a86807a76d972ae1393c5fc1be82aea8b0bf75
Nathan Graule <solarliner@gmail.com>
2017-08-04 20:51:14 +0700

    Porting code to TypeScript, part 2

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-import { exec } from "childprocess";
+import { exec } from "child-process-promise";
 import { inspect } from "util";
+
+/**
+ * Defines an action button
+ */
+interface Action {
+    id: string;
+    name: string;
+}
+
+interface AppData {
+    app_name: string;
+    title: string;
+    subtitle: string;
+    body: string;
+    isReply: boolean;
+    replyRecipient: string;
+    actions: Action[]
+}
+
+class LibNotify {
+    data: object;
+    constructor(appname: string, body: string, title: stirng, subtitle: string) {
+        this.data
+    }
+}
