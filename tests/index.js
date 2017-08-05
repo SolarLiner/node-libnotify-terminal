6538f114d72f7bd971858e3c1d6ee731d4504dfe
Nathan Graule <solarliner@gmail.com>
2017-08-05 15:44:35 +0700

    Completed tests

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,8 @@
 "use strict";
-exports.__esModule = true;
 
 var Notification = require("../").Notification;
 var assert = require("chai").assert;
-describe('Notification declaration', function () {
+describe('The Notification class', function () {
     it('should properly instanciate itself', function () {
         var n = new Notification('test', 'This is a test');
         // expect(n.app_name, 'n.app_name').equals('test', 'is set properly');
@@ -18,11 +17,11 @@ describe('Notification declaration', function () {
         // expect(n.title, 'notification title').equals("Title", 'to be properly set');
         // expect(n.subtitle, 'notification subtitle').equals('Subtitle', "to be properly set");
         // expect(n.body, 'notification body').equals('This is the body', 'to be properly set');
-        assert.isNotNull(n.title, 'notification title has been defined');
-        assert.isNotNull(n.subtitle, 'notification subtitle title has been defined');
-        assert.strictEqual(n.title, 'Title', 'notification title is properly set');
-        assert.strictEqual(n.subtitle, 'Subtitle', "notification subtitle to be properly set");
-        assert.strictEqual(n.body, 'This is the body', "notification body has been properly set");
+        assert.isNotNull(n.title);
+        assert.isNotNull(n.subtitle);
+        assert.strictEqual(n.title, 'Title');
+        assert.strictEqual(n.subtitle, 'Subtitle');
+        assert.strictEqual(n.body, 'This is the body');
     });
     it('should accept adding actions', function () {
         var n = new Notification('test', 'this is a test');
@@ -32,18 +31,29 @@ describe('Notification declaration', function () {
         };
         n.add_action(act);
         // expect(n.actions, 'n.actions').contains(act, 'contains the defined action');
-        assert.isArray(n.actions, 'notifications actions are defined');
-        assert.include(n.actions, act, "notification actions contains newly defined action");
+        assert.isArray(n.actions);
+        assert.include(n.actions, act);
+    });
+    it('should throw when adding spaces in the action id', () => {
+        var n = new Notification('test', 'test');
+        
     });
     it('should throw when setting reply details without setting reply mode first', function () {
         var n = new Notification('test', 'test');
         // expect(n.set_reply_message("Message content"), 'setting reply content throws').throws();
-        assert["throw"](n.set_reply_message, "can't update message");
+        assert.throw(() => {
+            n.set_reply_message("Message content");
+        }, "Should set notification as reply before setting message");
     });
     it('should be okay setting reply mode then editing reply message', function () {
         var n = new Notification('test', 'test');
         // expect(n.set_reply_message("New message content"), 'set new message content').ok();
-        assert.ok(n.set_reply('Recipient', 'Message content to be overriden'), "Setting reply mode");
-        assert.ok(n.set_reply_message("New message content"), "set new message content");
+        // assert.ok(n.set_reply('Recipient', 'Message content to be overriden'));
+        // assert.ok(n.set_reply_message("New message content"));
+        assert.doesNotThrow(() => {
+            n.set_reply('Recipient', 'Message to be overriden');
+            n.set_reply_message('New message');
+        })
+        assert.equal(n.isReply, true);
     });
 });
